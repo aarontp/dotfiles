583ee9fc91fcdde5a00dc902aceabab8212cc659
Aaron Peterson <aaronp@gmail.com>
2021-11-18 16:27:05 -0800

    Add VS Code autorandr and i3 configs

---
+++ .config/autorandr/move-left.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+export AUTORANDR_CURRENT_PROFILE="laptop"
+cd $( dirname $0 )
+~/.config/autorandr/postswitch
---
+++ .config/autorandr/move-right.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+export AUTORANDR_CURRENT_PROFILE="docked"
+~/.config/autorandr/postswitch
